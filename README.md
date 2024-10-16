e81db73eff95f98e6ddf87bfd3506f706f6e44f4
jekthw <mzakky.wijaya@gmail.com>
2024-10-16 20:44:20 +0700

    Index Overhaul, Theme, 'login.css' Experimental Change
    + Overhauled 'index.html'
    + Added theme colors to 'tailwind.css' for class usage
     'tq-midnight'
     'tq-dblue'
     'tq-blue'
     'tq-cyan'
     'tq-dcyan'
     'tq-teal'
     'tq-mint'
     'tq-lmint'

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Zakky Zakky
 Tabriz Tabriz
+
+# BRAND COLOR
+hsla(215, 45%, 40%, 1) 	#385e94
+hsla(164, 25%, 56%, 1)	#73ab9c 
+hsla(137, 54%, 72%, 1)	#91dea7 
+hsla(102, 100%, 91%, 1)	#dfffd1
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./html/index.html","./html/cssbuild.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tq-midnight' : '#05162e',
+        'tq-dblue': '#17254e',
+        'tq-blue': '#385e94',
+        'tq-dcyan':'#2aa887',
+        'tq-cyan': '#73ab9c',
+        'tq-teal': '#91dea7',
+        'tq-mint': '#dfffd1',
+        'tq-lmint':'#f0ffdc'
+
+
+      }
+      
+    },
   },
   plugins: [],
 }
