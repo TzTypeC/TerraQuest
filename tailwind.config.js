1c4aceb05d2e18b797bc70e38d8e4646d1bf1344
jekthw <mzakky.wijaya@gmail.com>
2024-10-17 08:11:08 +0700

    Profile Icon, Theme Adjusment
    + Added profile.svg
    + Added profile icon to 'index.html'
    + Added new colors to 'tailwind.config.js'
     'tq-vlmint' for Background
    + Changed existing colors to be more vibrant
     'tq-lmint' 'tq-teal'

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
         'tq-midnight' : '#05162e',
         'tq-dblue': '#17254e',
         'tq-blue': '#385e94',
+        // JANGAN PAKE CYAN GAK GUNA
         'tq-dcyan':'#2aa887',
         'tq-cyan': '#73ab9c',
-        'tq-teal': '#91dea7',
+        // JANGAN PAKE CYAN GAK GUNA
+        'tq-teal': '#abffab',
         'tq-mint': '#dfffd1',
-        'tq-lmint':'#f0ffdc'
+        'tq-lmint':'#f0ffdc',
+        'tq-vlmint':'#fcfffa'
 
 
       }
