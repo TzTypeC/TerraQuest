65e719d1e1c78e0027141cea753d6709dc2305d1
TzTypeC <tabriztypec2247@gmail.com>
2024-10-22 08:30:49 +0700

    Comppres User File

--- app/post.js
+++ app/post.js
@@ -1,5 +1,6 @@
 import { newPost } from './firestore.js';
 
+
 const submitPostButton = document.getElementById('submitPostButton');
 
 function readFileAsBase64(file) {
@@ -15,6 +16,36 @@ function readFileAsBase64(file) {
     });
 }
 
+// Fungsi untuk mengompres dan membaca file sebagai base64
+function compressAndReadFile(file) {
+    return new Promise((resolve, reject) => {
+        new Compressor(file, {
+            quality: 0.8, // Kualitas kompresi (0 sampai 1)
+            maxWidth: 1920, // Atur resolusi maksimum (opsional)
+            success(result) {
+                // Jika hasilnya lebih besar dari 1MB, turunkan kualitasnya
+                if (result.size > 1 * 1024 * 1024) {
+                    new Compressor(result, {
+                        quality: 0.6,
+                        success(compressedFile) {
+                            readFileAsBase64(compressedFile).then(resolve).catch(reject);
+                        },
+                        error(err) {
+                            reject(err);
+                        }
+                    });
+                } else {
+                    readFileAsBase64(result).then(resolve).catch(reject);
+                }
+            },
+            error(err) {
+                reject(err);
+            }
+        });
+    });
+}
+
+
 submitPostButton.addEventListener("click", (event) => {
     event.preventDefault();
     const file1 = document.getElementById('img1').files[0];
@@ -24,17 +55,17 @@ submitPostButton.addEventListener("click", (event) => {
     const promises = [];
 
     if (file1) {
-        promises.push(readFileAsBase64(file1)); // Membaca file1
+        promises.push(compressAndReadFile(file1));
     }
 
+    // Jika ada file gambar kedua, kompres dan baca file
     if (file2) {
-        promises.push(readFileAsBase64(file2)); // Membaca file2
+        promises.push(compressAndReadFile(file2));
     }
 
     // Tunggu semua file dibaca dan kemudian upload
     Promise.all(promises).then((base64Files) => {
         // Panggil fungsi newData dengan base64 dari file1 dan file2
-        console.log(base64Files[0], " ", base64Files[2])
         newPost(postContentTitle, postContentDesc, base64Files[0], base64Files[1]);
     }).catch((error) => {
         console.error("Error reading files: ", error);
